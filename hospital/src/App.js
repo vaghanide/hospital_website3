ad7daee44c068a58bc4b9e5d5e98273188746c7b
vaghanide <vaghanideep31@gmail.com>
2022-08-27 00:16:27 -0700

    Home_withloading

--- hospital/src/App.js
+++ hospital/src/App.js
@@ -1,34 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './Conteinars/Home/Home';
-import About from './Conteinars/Home/About/About';
-import Appointment from './Conteinars/Appointment/Appointment';
-import Contact from './Conteinars/Home/Contact/Contact';
-import Departments from './Conteinars/Departments/Departments'
-import Doctors from './Conteinars/Doctors/Doctors';
-import { Route, Switch } from 'react-router-dom';
-import Headar from './Component/Hedar/Hedar';
-import Footer from './Component/Footer/Footer';
+// import Home from './Conteinars/Home/Home';
+// import About from './Conteinars/Home/About/About';
+// import Appointment from './Conteinars/Appointment/Appointment';
+// import Contact from './Conteinars/Home/Contact/Contact';
+// import Departments from './Conteinars/Departments/Departments'
+// import Doctors from './Conteinars/Doctors/Doctors';
+// import { Route, Switch } from 'react-router-dom';
+// import Headar from './Component/Hedar/Hedar';
+// import Footer from './Component/Footer/Footer';
+import { useEffect, useState } from 'react';
+ const Loadinghome = loading(Home)
+      useEffect(() => {
+      setloading(true);
+      setTimeout(() => {
+        setloading(false)
+      } , 2000);
+    },[])
 
 function App() {
+
+  const[loading , setloading] =useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-      <Switch>
-        <Route exact path={"/"} component={Home} />
-        <Route exact path={"/Departments"} component={Departments} />
-        <Route exact path={"/About"} component={About} />
-        <Route exact path={"/Doctors"} component={Doctors} />
-        <Route exact path={"/Contact"} component={Contact} />
-        <Route exact path={"/Appointment"} component={Appointment} />
-      </Switch>
-      <Footer/>
+    <Homewithloading 
+    loading ={loading}
+    />
     
- 
     
-      </header>
-    </div>
-  );
+
+  )
 }
 
 export default App;
