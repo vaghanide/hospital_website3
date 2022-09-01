15b22ee3a006d7966695e31274735f7881f5d9bf
vaghanide <vaghanideep31@gmail.com>
2022-09-01 08:59:33 -0700

    signup/loading

--- hospital/src/container/Appointment/Appointment.js
+++ hospital/src/container/Appointment/Appointment.js
@@ -1,64 +1,263 @@
-import React from 'react';
+import React from "react";
 
 function Appointment(props) {
-    return (
-        <div>
-            <section id="appointment" className="appointment">
-                <div className="container">
-                    <div className="section-title">
-                        <h2>Make an Appointment</h2>
-                        <p>Aenean enim orci, suscipit vitae sodales ac, semper in ex. Nunc aliquam eget nibh eu euismod. Donec dapibus
-                            blandit quam volutpat sollicitudin. Fusce tincidunt sit amet ex in volutpat. Donec lacinia finibus tortor.
-                            Curabitur luctus eleifend odio. Phasellus placerat mi et suscipit pulvinar.</p>
-                    </div>
-                    <form action method="post" role="form" className="php-email-form">
-                        <div className="row">
-                            <div className="col-md-4 form-group">
-                                <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                                <div className="validate" />
-                            </div>
-                            <div className="col-md-4 form-group mt-3 mt-md-0">
-                                <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                                <div className="validate" />
-                            </div>
-                            <div className="col-md-4 form-group mt-3 mt-md-0">
-                                <input type="tel" className="form-control" name="phone" id="phone" placeholder="Your Phone" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                                <div className="validate" />
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-md-4 form-group mt-3">
-                                <input type="datetime" name="date" className="form-control datepicker" id="date" placeholder="Appointment Date" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                                <div className="validate" />
-                            </div>
-                            <div className="col-md-4 form-group mt-3">
-                                <select name="department" id="department" className="form-select">
-                                    <option value>Select Department</option>
-                                    <option value="Department 1">Department 1</option>
-                                    <option value="Department 2">Department 2</option>
-                                    <option value="Department 3">Department 3</option>
-                                </select>
-                                <div className="validate" />
-                            </div>
-                        </div>
-                        <div className="form-group mt-3">
-                            <textarea className="form-control" name="message" rows={5} placeholder="Message (Optional)" defaultValue={""} />
-                            <div className="validate" />
-                        </div>
-                        <div className="mb-3">
-                            <div className="loading">Loading</div>
-                            <div className="error-message" />
-                            <div className="sent-message">Your appointment request has been sent successfully. Thank you!</div>
-                        </div>
-                        <div className="text-center"><button type="submit">Make an Appointment</button></div>
-                    </form>
-                </div>
-            </section>
-
+  return (
+    <div>
+      <meta charSet="utf-8" />
+      <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+      <title>Medilab Bootstrap Template - Index</title>
+      <meta content name="description" />
+      <meta content name="keywords" />
+      <link
+        href="../assets/vendor/animate.css/animate.min.css"
+        rel="stylesheet"
+      />
+      <link
+        href="../assets/vendor/bootstrap/css/bootstrap.min.css"
+        rel="stylesheet"
+      />
+      <link
+        href="../assets/vendor/bootstrap-icons/bootstrap-icons.css"
+        rel="stylesheet"
+      />
+      <link
+        href="../assets/vendor/boxicons/css/boxicons.min.css"
+        rel="stylesheet"
+      />
+      <link
+        href="../assets/vendor/fontawesome-free/css/all.min.css"
+        rel="stylesheet"
+      />
+      <link
+        href="../assets/vendor/glightbox/css/glightbox.min.css"
+        rel="stylesheet"
+      />
+      <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet" />
+      <link
+        href="../assets/vendor/swiper/swiper-bundle.min.css"
+        rel="stylesheet"
+      />
+      <link href="../assets/css/style.css" rel="stylesheet" />
+      <div className="main-header">
+        <div id="topbar" className="d-flex align-items-center fixed-top">
+          <div className="container d-flex justify-content-between">
+            <div className="contact-info d-flex align-items-center">
+              <i className="bi bi-envelope" />{" "}
+              <a href="mailto:contact@example.com">cityhospital@example.com</a>
+              <i className="bi bi-phone" /> +91 9988776655
+            </div>
+            <div className="d-none d-lg-flex social-links align-items-center">
+              <a href="#" className="twitter">
+                <i className="bi bi-twitter" />
+              </a>
+              <a href="#" className="facebook">
+                <i className="bi bi-facebook" />
+              </a>
+              <a href="#" className="instagram">
+                <i className="bi bi-instagram" />
+              </a>
+              <a href="#" className="linkedin">
+                <i className="bi bi-linkedin" />
+              </a>
+            </div>
+          </div>
         </div>
-    );
+        <header id="header" className="fixed-top">
+          <div className="container d-flex align-items-center">
+            <div className="logo">
+              <a href="../index.html">
+                <h1 className="logo me-auto">City</h1>
+                <br />
+                <h2 className="logo-tiny-text me-auto">
+                  Multispeciality Hospital
+                </h2>
+              </a>
+            </div>
+            <nav id="navbar" className="navbar order-last order-lg-0">
+              <ul>
+                <li>
+                  <a className="nav-link scrollto" href="../index.html">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a className="nav-link scrollto" href="./departments.html">
+                    Departments
+                  </a>
+                </li>
+                <li>
+                  <a className="nav-link scrollto" href="./doctors.html">
+                    Doctors
+                  </a>
+                </li>
+                <li>
+                  <a className="nav-link scrollto" href="./about.html">
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a className="nav-link scrollto" href="./contact.html">
+                    Contact
+                  </a>
+                </li>
+              </ul>
+              <i className="bi bi-list mobile-nav-toggle" />
+            </nav>
+            <a href="./appointment.html" className="appointment-btn scrollto">
+              <span className="d-none d-md-inline">Make an</span>
+              Appointment
+            </a>
+          </div>
+        </header>
+      </div>
+      <section id="appointment" className="appointment">
+        <div className="container">
+          <div className="section-title">
+            <h2>Make an Appointment</h2>
+            <p>
+              Aenean enim orci, suscipit vitae sodales ac, semper in ex. Nunc
+              aliquam eget nibh eu euismod. Donec dapibus blandit quam volutpat
+              sollicitudin. Fusce tincidunt sit amet ex in volutpat. Donec
+              lacinia finibus tortor. Curabitur luctus eleifend odio. Phasellus
+              placerat mi et suscipit pulvinar.
+            </p>
+          </div>
+          <form action method="post" role="form" className="php-email-form">
+            <div className="row">
+              <div className="col-md-4 form-group">
+                <input
+                  type="text"
+                  name="name"
+                  className="form-control"
+                  id="name"
+                  placeholder="Your Name"
+                  data-rule="minlen:4"
+                  data-msg="Please enter at least 4 chars"
+                />
+                <div className="validate" />
+              </div>
+              <div className="col-md-4 form-group mt-3 mt-md-0">
+                <input
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  id="email"
+                  placeholder="Your Email"
+                  data-rule="email"
+                  data-msg="Please enter a valid email"
+                />
+                <div className="validate" />
+              </div>
+              <div className="col-md-4 form-group mt-3 mt-md-0">
+                <input
+                  type="tel"
+                  className="form-control"
+                  name="phone"
+                  id="phone"
+                  placeholder="Your Phone"
+                  data-rule="minlen:4"
+                  data-msg="Please enter at least 4 chars"
+                />
+                <div className="validate" />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-4 form-group mt-3">
+                <input
+                  type="datetime"
+                  name="date"
+                  className="form-control datepicker"
+                  id="date"
+                  placeholder="Appointment Date"
+                  data-rule="minlen:4"
+                  data-msg="Please enter at least 4 chars"
+                />
+                <div className="validate" />
+              </div>
+              <div className="col-md-4 form-group mt-3">
+                <select
+                  name="department"
+                  id="department"
+                  className="form-select"
+                >
+                  <option value>Select Department</option>
+                  <option value="Department 1">Department 1</option>
+                  <option value="Department 2">Department 2</option>
+                  <option value="Department 3">Department 3</option>
+                </select>
+                <div className="validate" />
+              </div>
+            </div>
+            <div className="form-group mt-3">
+              <textarea
+                className="form-control"
+                name="message"
+                rows={5}
+                placeholder="Message (Optional)"
+                defaultValue={""}
+              />
+              <div className="validate" />
+            </div>
+            <div className="mb-3">
+              <div className="loading">Loading</div>
+              <div className="error-message" />
+              <div className="sent-message">
+                Your appointment request has been sent successfully. Thank you!
+              </div>
+            </div>
+            <div className="text-center">
+              <button type="submit">Make an Appointment</button>
+            </div>
+          </form>
+        </div>
+      </section>
+      <footer id="footer">
+        <div className="container d-md-flex py-4">
+          <div className="me-md-auto text-center text-md-start">
+            <div>
+              <h4>Address</h4>
+              <p>
+                F-505, <br />
+                Inovative Plazza
+                <br />
+                New Delhi, India
+                <br />
+                <br />
+                <strong>Phone:</strong> +91 9988776655
+                <br />
+                <strong>Email:</strong> cityhospital@example.com
+                <br />
+              </p>
+            </div>
+          </div>
+          <div className="social-links text-center text-md-right pt-3 pt-md-0">
+            <a href="#" className="twitter">
+              <i className="bx bxl-twitter" />
+            </a>
+            <a href="#" className="facebook">
+              <i className="bx bxl-facebook" />
+            </a>
+            <a href="#" className="instagram">
+              <i className="bx bxl-instagram" />
+            </a>
+            <a href="#" className="google-plus">
+              <i className="bx bxl-skype" />
+            </a>
+            <a href="#" className="linkedin">
+              <i className="bx bxl-linkedin" />
+            </a>
+          </div>
+        </div>
+      </footer>
+      <div id="preloader" />
+      <a
+        href="#"
+        className="back-to-top d-flex align-items-center justify-content-center"
+      >
+        <i className="bi bi-arrow-up-short" />
+      </a>
+    </div>
+  );
 }
-
-
 
 export default Appointment;
