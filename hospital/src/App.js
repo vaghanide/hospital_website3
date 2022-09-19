aadec71c9a3b4787f7a6f24759f5fd718ef69c3c
vaghanide <vaghanideep31@gmail.com>
2022-09-19 10:44:33 +0530

    Admin

--- hospital/src/App.js
+++ hospital/src/App.js
@@ -10,23 +10,28 @@ import { Route, Router, Switch } from 'react-router-dom';
 import Headar from './Component/Hedar/Hedar';
  import Footer from './Component/Footer/Footer';
 import List from './componet/List';
+import Layout from './admin/Componet/Layout';
+import MedcinesAdmin from './admin/Contanier/MedcinesAdmin';
 
 
 function App() {
   return (
     <div>
     <header />
-    <switch>
-      {/* <Router exact path={"/"} componet={Home}/>
+    {/* <switch>
+      {/* { <Router exact path={"/"} componet={Home}/>
       <Router exact path={"/appointment"} componet={appointment}/>
       <Router exact path={"/about"} componet={about}/>
-      <Router exact path={"/contact"} componet={contact}/> */}
+      <Router exact path={"/contact"} componet={contact}/> }
       <Madicines />
-      <List />
-
-
-    </switch>
-    <footer />
+      <List /> */}
+{/* <switch/>
+    <footer />     */}
+    <Layout>
+      <switch>
+        <Route exact path={"/MedcinesAdmin"} componet={MedcinesAdmin}/>
+      </switch>
+    </Layout>
     </div>
     
   )
